45c34a6c70663ebfb373720f623025f304de13bb
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-25 13:44:22 +0000

    Updating quality of all types of items moved to separate private methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,37 +10,50 @@ class GildedRose
     @items.each do |item|
       case item.name
         when "Aged Brie"
-          item.quality = item.quality + 1 unless item.quality >= 50
+          update_aged_brie_quality(item)
         when "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in > 10 && item.quality < 50
-            item.quality += 1
-          elsif item.sell_in > 6 && item.quality < 50
-            item.quality += 2
-          elsif item.sell_in > 0 && item.quality < 50
-             item.quality += 3
-          else item.quality = 0
-          end
+          update_backstage_passes_quality(item)
         when "Sulfuras, Hand of Ragnaros"; nil
         when "Conjured"
-          if item.sell_in > 0
-            item.quality -= 2
-          else item.quality -= 4
-          end
+          update_conjured_quality(item)
         else
-          if item.sell_in > 0
-            item.quality -= 1
-          else
-            item.quality -= 2
-          end
+          update_regular_item_quality(item)
         end
-
       ensure_quality_is_not_negative(item)
       reduce_sell_in(item)
-
     end
   end
 
   private
+  def update_aged_brie_quality(item)
+    item.quality = item.quality + 1 unless item.quality >= 50
+  end
+
+  def update_backstage_passes_quality(item)
+    if item.sell_in > 10 && item.quality < 50
+      item.quality += 1
+    elsif item.sell_in > 6 && item.quality < 50
+      item.quality += 2
+    elsif item.sell_in > 0 && item.quality < 50
+       item.quality += 3
+    else item.quality = 0
+    end
+  end
+
+  def update_conjured_quality(item)
+    if item.sell_in > 0
+      item.quality -= 2
+    else item.quality -= 4
+    end
+  end
+
+  def update_regular_item_quality(item)
+    if item.sell_in > 0
+      item.quality -= 1
+    else
+      item.quality -= 2
+    end
+  end
 
   def ensure_quality_is_not_negative(item)
     item.quality = 0  if item.quality < 0
